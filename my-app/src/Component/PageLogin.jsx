d985db6b6ac72362dabf2cf66c6af59c6443441c
YouBeer-gif <PhicharatMns@gmail.com>
2025-07-27 16:39:29 +0700

    data

--- my-app/src/Component/PageLogin.jsx
+++ my-app/src/Component/PageLogin.jsx
@@ -1,4 +1,21 @@
+import { elements } from "chart.js";
+import { use, useState } from "react";
+import { useNavigate } from "react-router-dom"; //ใช้สําหรับเข้าถึง Roter one
+
 export default function PageLogin() {
+  const [Email, setEmail] = useState("Beer");
+  const [Password, setPassword] = useState("12345");
+  const navigate = useNavigate(); 
+  const [error, setError] = useState("");
+
+  const check = () => {
+    if (Email === "Beer" && Password === "12345") {
+      navigate("/");
+    } else {
+      setError('ไม่ผ่านระบบ');
+    }
+  };
+
   return (
     <div className="h-screen">
       <div className="w-fit mx-auto ">
@@ -8,18 +25,39 @@ export default function PageLogin() {
           <div className="text-center py-5">
             <div className="pb-5">
               <input
+                type="email"
+                value={Email}
+                onChange={(element) => setEmail(element.target.value)}
                 className="border w-85 rounded-xl h-9 pl-2"
                 placeholder="Email"
               ></input>
             </div>
             <div className="pb-5">
               <input
+                type="password"
+                onChange={(elements) => setPassword(elements.target.value)}
+                value={Password}
                 className="border w-85 rounded-xl h-9 pl-2"
                 placeholder="Password"
               ></input>
             </div>
-            <p className='text-purple-500 text-body pb-5'>Forgot your password?</p>
-            <button className="w-85 rounded-xl h-9 pl-2 bg-purple-500 font-bold">Login</button>
+            <div>
+              {" "}
+              <span className="text-purple-500 text-body pb-5 cursor-pointer">
+                Forgot your password?
+              </span>
+            </div>
+            <button
+              onClick={check}
+              className="w-85 rounded-xl h-9 pl-2 mt-5 bg-purple-500 font-bold"
+            >
+              Login
+            </button>
+            {error && (
+              <p className="mt-4 text-sm font-medium text-center text-red-500">
+                {error}
+              </p>
+            )}
           </div>
         </div>
       </div>
